7bebac95db17f00d541663d594a7ac8b17d8db46
Irving Lop <irving.dev.mobi@gmail.com>
2023-07-18 15:41:36 -0600

    U5 -> P2 -> 2 Add repository and Manual Dependency Injection

--- app/src/main/java/com/example/marsphotos/ui/MarsPhotosApp.kt
+++ app/src/main/java/com/example/marsphotos/ui/MarsPhotosApp.kt
@@ -46,7 +46,7 @@ fun MarsPhotosApp() {
                 .fillMaxSize()
                 .padding(it)
         ) {
-            val marsViewModel: MarsViewModel = viewModel()
+            val marsViewModel: MarsViewModel = viewModel(factory = MarsViewModel.Factory)
             HomeScreen(marsUiState = marsViewModel.marsUiState)
         }
     }
